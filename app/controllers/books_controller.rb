0a565613543e837d9d6dfd267fce744c8f7d1d17
zecky1120 <takahide1120@gmail.com>
2024-03-19 18:04:18 +0900

    fix: rubocop修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.order("created_at DESC").page(params[:page]).per(1)
+    @books = Book.order('created_at DESC').page(params[:page]).per(1)
   end
 
   # GET /books/1 or /books/1.json
