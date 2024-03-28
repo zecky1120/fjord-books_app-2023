3a73b7f8ce2f43508151c91a30401b0c7fc0b724
zecky1120 <takahide1120@gmail.com>
2024-03-28 18:30:02 +0900

    add: ユーザー情報、編集、登録の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "books#index"
   devise_for :users
+  resources :users, only: %i[index show]
   resources :books
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_26_062355) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_28_064335) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,6 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_26_062355) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "zipcode"
+    t.string "address"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
