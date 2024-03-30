83fa130886e981efed7b4eb27481cb33fdad24ce
zecky1120 <takahide1120@gmail.com>
2024-03-30 20:36:56 +0900

    add: users一覧ページにページネーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).per(2)
   end
 
   def show
