66c37c6a2eadb835d63f9e81db26f250adf96ad0
zecky1120 <takahide1120@gmail.com>
2024-04-01 12:21:27 +0900

    add: letter_opener_webの実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: %i[index show]
   resources :books
-
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
