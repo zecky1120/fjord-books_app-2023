cd6100bd01c1fdad2efb3b03db85e2691f51cdb6
zecky1120 <takahide1120@gmail.com>
2024-04-19 12:52:25 +0900

    add: reportscontrollerにnoticeを追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,7 +13,7 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
     @report.user_id = current_user.id
     if @report.save
-      redirect_to @report
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new
     end
@@ -30,7 +30,7 @@ class ReportsController < ApplicationController
   def update
     @report = Report.find(params[:id])
     if @report.update(report_params)
-      redirect_to @report
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit
     end
@@ -39,7 +39,7 @@ class ReportsController < ApplicationController
   def destroy
     @report = Report.find(params[:id])
     if @report.destroy
-      redirect_to reports_path
+      redirect_to reports_path, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
     else
       render :show
     end
