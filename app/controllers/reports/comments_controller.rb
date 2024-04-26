3c8c45a60ebef2e11e868f9ae42685e507e3bbe1
zecky1120 <takahide1120@gmail.com>
2024-04-26 14:55:07 +0900

    add: reports/comment_controllerを追加

---
+++ app/controllers/reports/comments_controller.rb
@@ -0,0 +1,13 @@
+class Reports::CommentsController < CommentsController
+  before_action :set_commentable
+
+  private
+
+  def set_commentable
+    @commentable = Report.find(params[:report_id])
+  end
+
+  def render_commentable_show
+    render 'reports/show'
+  end
+end
