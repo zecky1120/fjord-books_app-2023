da12f614c5c18e0ecad107a5b5d894d5a5853b24
zecky1120 <takahide1120@gmail.com>
2024-05-06 19:35:36 +0900

    fix: コメント一覧にorderを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
 
   # GET /books/1 or /books/1.json
   def show
-    @comments = @book.comments
+    @comments = @book.comments.order(id: :desc)
     @comment = Comment.new
   end
 
