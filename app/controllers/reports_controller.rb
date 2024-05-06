dd504bc6e114923635b7891397f17396c5d71bde
zecky1120 <takahide1120@gmail.com>
2024-05-06 19:45:36 +0900

    fix: reportの作成メソッドを修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,8 +10,7 @@ class ReportsController < ApplicationController
   end
 
   def create
-    @report = Report.new(report_params)
-    @report.user = current_user
+    @report = current_user.reports.new(report_params)
     if @report.save
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
