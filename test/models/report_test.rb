6b2a89559e4e456d2ac9a704f71309e099743c12
zecky1120 <takahide1120@gmail.com>
2024-07-06 20:59:39 +0900

    add: report_testの実装一旦完了

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,12 +3,30 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # user = User.create!(name: 'Alice', email: 'alice@example.com', password: 'password')
-  # report = Report.create!(title: 'テスト入力', content: 'テキストが入ります。', user_id: user.id)
-  # test "editable?" do
-  #   assert report.editable?(user)
-  # end
-  # test "created_on" do
-  #   assert '2024/06/24', report.created_on
-  # end
+  test "editable?" do
+    alice = users(:alice)
+    bob = users(:bob)
+    alice_report = reports(:alice)
+    bob_report = reports(:bob)
+    assert alice_report.editable?(alice)
+    assert_not alice_report.editable?(bob)
+  end
+  test "created_on" do
+    alice_report = reports(:alice)
+    report_created_at = alice_report.created_at
+    assert_equal report_created_at.strftime("%Y/%m/%d"), I18n.l(alice_report.created_on)
+  end
+
+  test "save_mentions" do
+    alice_report = reports(:alice)
+    bob = users(:bob)
+    bob_report = reports(:bob)
+    bob_report = bob.reports.create!(title: 'アリスの記事を言及している', content: "http://localhost:3000/reports/#{alice_report.id}")
+    assert_includes(alice_report.mentioned_reports, bob_report)
+    assert_not_includes(bob_report.mentioned_reports, alice_report)
+    assert bob_report.mentioning_reports.find(alice_report.id)
+    update_content = '言及日報のURLを削除'
+    bob_report.update(title: '上書きしたよ', content: update_content)
+    assert_not_includes(bob_report.mentioned_reports, alice_report)
+  end
 end
