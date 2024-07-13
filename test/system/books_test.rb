c7e04e041923bc65bcd3a0106178899abdb2546c
zecky1120 <takahide1120@gmail.com>
2024-07-13 20:13:57 +0900

    fix: 空行削除

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -12,40 +12,31 @@ class BooksTest < ApplicationSystemTestCase
     click_on 'ログイン'
     assert_text 'ログインしました'
   end
-
   test '本の一覧表示される' do
     visit books_url
     assert_selector 'h1', text: '本の一覧'
   end
-
   test '本の新規作成ができる' do
     visit books_url
     click_on '本の新規作成'
-
     fill_in 'メモ', with: @book.memo
     fill_in 'タイトル', with: @book.title
     click_on '登録する'
-
     assert_text '本が作成されました。'
     click_on '本の一覧に戻る'
   end
-
   test '本の編集ができる' do
     visit book_url(@book)
     click_on 'この本を編集', match: :first
-
     fill_in 'メモ', with: @book.memo
     fill_in 'タイトル', with: @book.title
     click_on '更新する'
-
     assert_text '本が更新されました。'
     click_on '本の一覧に戻る'
   end
-
   test '本を削除できる' do
     visit book_url(@book)
     click_on 'この本を削除', match: :first
-
     assert_text '本が削除されました。'
   end
 end
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -12,40 +12,31 @@ class ReportsTest < ApplicationSystemTestCase
     click_on 'ログイン'
     assert_text 'ログインしました'
   end
-
   test '日報の一覧表示される' do
     visit reports_url
     assert_selector 'h1', text: '日報の一覧'
   end
-
   test '日報の新規作成できる' do
     visit reports_url
     click_on '日報の新規作成'
-
     fill_in 'タイトル', with: @report.title
     fill_in '内容', with: @report.content
     click_on '登録する'
-
     assert_text '日報が作成されました。'
     click_on '日報の一覧に戻る'
   end
-
   test '日報の編集ができる' do
     visit report_url(@report)
     click_on 'この日報を編集', match: :first
-
     fill_in 'タイトル', with: '日報タイトルの変更'
     fill_in '内容', with: '日報内容の変更'
     click_on '更新する'
-
     assert_text '日報が更新されました。'
     click_on '日報の一覧に戻る'
   end
-
   test '日報の削除ができる' do
     visit report_url(@report)
     click_on 'この日報を削除', match: :first
-
     assert_text '日報が削除されました。'
   end
 end
