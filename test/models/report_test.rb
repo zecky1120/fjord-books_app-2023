b7d70036508a27bd1bbb2eadeab13068528f9ebd
zecky1120 <takahide1120@gmail.com>
2024-07-13 16:19:32 +0900

    fix: rubocop対応

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,24 +3,20 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test "editable?" do
+  test 'editable?' do
     alice = users(:alice)
     bob = users(:bob)
     alice_report = reports(:alice)
-    bob_report = reports(:bob)
     assert alice_report.editable?(alice)
     assert_not alice_report.editable?(bob)
   end
-  test "created_on" do
+  test 'created_on' do
     alice_report = reports(:alice)
-    report_created_at = alice_report.created_at
-    assert_equal report_created_at.strftime("%Y/%m/%d"), I18n.l(alice_report.created_on)
+    assert_equal alice_report.created_at.strftime('%Y/%m/%d'), I18n.l(alice_report.created_on)
   end
-
-  test "save_mentions" do
+  test 'save_mentions' do
     alice_report = reports(:alice)
     bob = users(:bob)
-    bob_report = reports(:bob)
     bob_report = bob.reports.create!(title: 'アリスの記事を言及している', content: "http://localhost:3000/reports/#{alice_report.id}")
     assert_includes(alice_report.mentioned_reports, bob_report)
     assert_not_includes(bob_report.mentioned_reports, alice_report)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "name_or_email" do
+  test 'name_or_email' do
     alice = users(:alice)
     carol = users(:carol)
     assert_equal 'アリス', alice.name_or_email
